f367803e1b8eaafb7451bf9cf523b4c6b4869ee7
Greg Cooper <853085+awfy@users.noreply.github.com>
2020-05-07 16:48:09 -0700

    Actual

--- js/script.js
+++ js/script.js
@@ -1,100 +1,102 @@
 const list = document.querySelectorAll('.student-item');
 const itemsPerPage = 10; // Set how many items to show per page.
-const initialPageNum = 1; // Set which page number loads first.
-
-const pageHeader = document.querySelector('.page-header');
-const title = pageHeader.querySelector('h2');
-const searchBox = document.createElement('div');
-const searchInput = document.createElement('input');
-const searchButton = document.createElement('button');
-searchBox.className = 'student-search';
-searchBox.appendChild(searchInput);
-searchBox.appendChild(searchButton);
-searchInput.placeholder = 'Search for students...';  
-searchInput.type = 'search';
-searchButton.textContent = 'Search';
-pageHeader.append(searchBox);
+const initialPage = 1; // Which page is loaded in first.
 
 /**
- * 
- * @param {*} list 
- * @param {*} query 
- * @param {*} page 
- */
-const showPage = (list, query, page) => {
-  let startIndex = page * itemsPerPage - itemsPerPage;
+ * Function for traversing the list's DOM and applying the required display 
+ * styles to the relevant items.
+**/
+const showPage = (list, page) => {
+
+  /**
+   * We take the current page multiplied by the items per page which will give 
+   * us the total number of items the user will have seen in the list then 
+   * minus a further items per page so that it minuses out to the first index 
+   * of the page.
+  **/
+  let startIndex = page * itemsPerPage - itemsPerPage; 
+
+  /**
+   * Continuing on from the `startIndex` logic, we simply don't apply the last 
+   * minus part of the sum which gives us the index of the items which will 
+   * follow on from the items per page that are visible.
+  **/
   let endIndex = page * itemsPerPage;
-  if (query) {
-    for (let i = 0; i < list.length; i += 1) {
-      const name = list[i].querySelector('h3');
-      const comparison = name.textContent.indexOf(query) !== -1;
-      if (comparison) {
-        list[i].style.display = '';
-      } else {
-        list[i].style.display = 'none';
-      }
-    }
-  } else {
-    for (let i = 0; i < list.length; i += 1) {
-      if (i >= startIndex && i < endIndex) {
-        list[i].style.display = '';
-      } else {
-        list[i].style.display = 'none';
-      }
+
+  /**
+   * For each item with an index that is between the start and end indexes 
+   * will get no display style applied, whilst the rest will recieve a none 
+   * value for display.
+  **/
+  for (let i = 0; i < list.length; i += 1) {
+    if (i >= startIndex && i < endIndex) {
+      list[i].style.display = '';
+    } else {
+      list[i].style.display = 'none';
     }
   }
+
 };
-showPage(list, null, initialPage); 
+
 
 /**
- * The event listener on `input` allows the user to see search results as they 
- * type instead of waiting for them to click the button. This automatically 
- * gives us the advantage of the input[type="search"]'s clear event returning 
- * all of the results back to normal.
- */
-searchBox.addEventListener('input', (e) => {
-  showPage(list, searchInput.value);
-});
-
-/**
- * In addition to the listener on `input`, we include one of `click` which 
- * targets the "Search" button. If for any reason the input event fails to 
- * trigger this is backup.
- */
-searchBox.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON'){
-    showPage(list, searchInput.value);
-  }
-});
-
+ * For each item with an index that is between the start and end indexes 
+ * will get no display style applied, whilst the rest will recieve a none 
+ * value for display.
+**/
 const appendPageLinks = (list) => {
+
   const page = document.querySelector('.page');
   const pagination = document.createElement('div');
   pagination.className = 'pagination';
   const paginationUL = document.createElement('ul');
   pagination.appendChild(paginationUL);
-  const paginationPageCount = list.length / itemsPerPage;
+
+  /**
+   * Sum to take the total length of the list and divide it by how many items 
+   * per page to work out how many pages there will be.
+  **/
+  const paginationPageCount = list.length / itemsPerPage; 
+
   for (let i = 0; i < paginationPageCount; i += 1) {
+
+    /**
+     * Constructs the pagination link along with the page number.
+    **/
     const paginationLI = document.createElement('li');
     paginationUL.appendChild(paginationLI);
     const paginationLink = document.createElement('a');
     paginationLI.appendChild(paginationLink);
     paginationLinkLabel = i + 1;
     paginationLink.textContent = paginationLinkLabel;
+
+    /**
+     * Apply active to the initial page that is loaded
+    **/
     if (paginationLinkLabel === initialPage) {
       paginationLink.className = 'active';
     }
+
+    /**
+     * Event listener to listen for clicks on the pagination links and remove 
+     * any existing active class then apply one to the clicked link.
+    **/
     pagination.addEventListener('click',  (e) => {
       if (e.target.tagName === 'A') {
         const paginationButton = e.target;
         for (let i = 0; i < paginationPageCount; i += 1) {
           paginationLink.className = '';
-          showPage(list, null, paginationButton.textContent);
+          showPage(list, paginationButton.textContent);
         }
         paginationButton.className = 'active';
       }
     });
+
   }
-  page.appendChild(pagination);
+
+  page.appendChild(pagination); // Append the pagination to the page.
+
 }
-appendPageLinks(list);
+
+showPage(list, initialPage); // Loads the initial page before any pagination has been clicked.
+appendPageLinks(list); // Displays the pagination.
