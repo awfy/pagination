a705b5ffab36ea13afa8ee55927ef48f8f26161b
Greg Cooper <853085+awfy@users.noreply.github.com>
2020-05-07 16:48:09 -0700

    Actual

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# pagination
- A simple way of paginating lists of users.
+# Pagination
+#### A Treehouse project repository
+
+This project demonstrates my ability to manipulate the DOM from the given `index.html`, `44students.html`, and `64students.html` files with Javascript in order to add pagination. I'll be utilizing `for` loops to both determine which students to display and how the pagination should be displaying (which link is active and how many links).
--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 const list = document.querySelectorAll('.student-item');
 const itemsPerPage = 10; // Set how many items to show per page.
-const initialPage = 1; // Set which page loads first.
+const initialPage = 1; // Which page is loaded in first.
 
-const pageHeader = document.querySelector('.page-header');
-const title = pageHeader.querySelector('h2');
-const searchBox = document.createElement('div');
-const searchInput = document.createElement('input');
-const searchButton = document.createElement('button');
-searchBox.className = 'student-search';
-searchBox.appendChild(searchInput);
-searchBox.appendChild(searchButton);
-searchInput.type = 'search';
-searchQuery = searchInput.value;
-searchInput.value = '';
-searchButton.type = 'submit';
-searchButton.textContent = 'Search';
-pageHeader.append(searchBox);
-
-searchBox.addEventListener('submit', (e) => {
-  e.target.preventDefault();
-});
-
-
+/**
+ * Function for traversing the list's DOM and applying the required display 
+ * styles to the relevant items.
+**/
 const showPage = (list, page) => {
-  let startIndex = page * itemsPerPage - itemsPerPage;
+
+  /**
+   * We take the current page multiplied by the items per page which will give 
+   * us the total number of items the user will have seen in the list then 
+   * minus a further items per page so that it minuses out to the first index 
+   * of the page.
+  **/
+  let startIndex = page * itemsPerPage - itemsPerPage; 
+
+  /**
+   * Continuing on from the `startIndex` logic, we simply don't apply the last 
+   * minus part of the sum which gives us the index of the items which will 
+   * follow on from the items per page that are visible.
+  **/
   let endIndex = page * itemsPerPage;
+
+  /**
+   * For each item with an index that is between the start and end indexes 
+   * will get no display style applied, whilst the rest will recieve a none 
+   * value for display.
+  **/
   for (let i = 0; i < list.length; i += 1) {
     if (i >= startIndex && i < endIndex) {
       list[i].style.display = '';
@@ -32,26 +35,52 @@ const showPage = (list, page) => {
       list[i].style.display = 'none';
     }
   }
-};
-showPage(list, initialPage);
 
+};
+
+
+/**
+ * For each item with an index that is between the start and end indexes 
+ * will get no display style applied, whilst the rest will recieve a none 
+ * value for display.
+**/
 const appendPageLinks = (list) => {
+
   const page = document.querySelector('.page');
   const pagination = document.createElement('div');
   pagination.className = 'pagination';
   const paginationUL = document.createElement('ul');
   pagination.appendChild(paginationUL);
-  const paginationPageCount = list.length / itemsPerPage;
+
+  /**
+   * Sum to take the total length of the list and divide it by how many items 
+   * per page to work out how many pages there will be.
+  **/
+  const paginationPageCount = list.length / itemsPerPage; 
+
   for (let i = 0; i < paginationPageCount; i += 1) {
+
+    /**
+     * Constructs the pagination link along with the page number.
+    **/
     const paginationLI = document.createElement('li');
     paginationUL.appendChild(paginationLI);
     const paginationLink = document.createElement('a');
     paginationLI.appendChild(paginationLink);
     paginationLinkLabel = i + 1;
     paginationLink.textContent = paginationLinkLabel;
+
+    /**
+     * Apply active to the initial page that is loaded
+    **/
     if (paginationLinkLabel === initialPage) {
       paginationLink.className = 'active';
     }
+
+    /**
+     * Event listener to listen for clicks on the pagination links and remove 
+     * any existing active class then apply one to the clicked link.
+    **/
     pagination.addEventListener('click',  (e) => {
       if (e.target.tagName === 'A') {
         const paginationButton = e.target;
@@ -62,7 +91,12 @@ const appendPageLinks = (list) => {
         paginationButton.className = 'active';
       }
     });
+
   }
-  page.appendChild(pagination);
+
+  page.appendChild(pagination); // Append the pagination to the page.
+
 }
-appendPageLinks(list);
+
+showPage(list, initialPage); // Loads the initial page before any pagination has been clicked.
+appendPageLinks(list); // Displays the pagination.
